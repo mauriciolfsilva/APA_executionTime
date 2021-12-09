a4406f385006520f1f6dd1dac96705052b1f822c
mauriciolfsilva <mauriciolfsilva@gmail.com>
2021-12-09 02:44:03 -0300

    Juntando com sorts

--- lib/myLib.c
+++ lib/myLib.c
@@ -19,6 +19,77 @@
  *seg_sistema_total = (seg_sistema + 0.000001 * mseg_sistema);
 }*/
 
+
+void trocar(int *var1, int *var2)
+{
+    int temp = *var1;
+    *var1 = *var2;
+    *var2 = temp;
+}
+
+
+void bubble_sort(int array[], int n) {
+     int  i, j;
+    for (i = 0; i < n-1; i++){
+        for (j = 0; j < n-i-1; j++){
+            if (array[j] > array[j+1]){
+                trocar(&array[j], &array[j+1]);
+            }
+        }
+    }    
+}
+
+void selection_sort (int array[],int n) {
+  int i, j, minino, auxiliar;
+  
+  for (i = 0; i < (n - 1); i++) {
+    minino = i;
+    for (j = i+1; j < n; j++) {
+      if (array[j] < array[minino]) {
+        minino = j;
+      }
+    }
+    if (i != minino) {
+      trocar(&array[i], &array[minino]);
+    }
+  }
+}
+
+
+
+void heapify(int array[], int n, int i)
+{
+    int esquerda = 2 * i + 1;
+    int direita = 2 * i + 2;
+    int maior = i;
+
+    if (direita < n && array[direita] > array[maior])
+        maior = direita;
+ 
+    if (esquerda < n && array[esquerda] > array[maior])
+        maior = esquerda;
+ 
+ 
+    if (maior != i) {
+        trocar(&array[i], &array[maior]);
+ 
+        heapify(array, n, maior);
+    }
+}
+void heap_sort(int array[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(array, n, i);
+ 
+    for (int i = n - 1; i > 0; i--) {
+        trocar(&array[0], &array[i]);
+ 
+        heapify(array, i, 0);
+    }
+}
+
+
+
 int * newArray(int n) {
     static int* array; 
     array = malloc(sizeof(int) * n);
@@ -38,16 +109,16 @@ int * callFunction(int *array, int idFunction, int size, double *seg_CPU_total_i
     switch (idFunction)
     {
     case 1:
-        /* bubblesort */
-        return NULL;
+        bubble_sort(customArray, size);
+        return customArray;
         break;
     case 2:
-        /* insertionsort */
-        return NULL;
+        selection_sort(customArray, size);
+        return customArray;
         break;
     case 3:
-        /* heapsort */
-        return NULL;
+        heap_sort(customArray, size);
+        return customArray;
         break;
     case 4:
         /* boxsort */
@@ -59,74 +130,3 @@ int * callFunction(int *array, int idFunction, int size, double *seg_CPU_total_i
         break;
     }
 }
-
-
-void trocar(int *var1, int *var2)
-{
-    int temp = *var1;
-    *var1 = *var2;
-    *var2 = temp;
-}
-
-
-void bubble_sort(int [] array, int n) {
-     int  i, j;
-    for (i = 0; i < n-1; i++){
-        for (j = 0; j < n-i-1; j++){
-            if (array[j] > array[j+1]){
-                trocar(array[j], array[j+1]);
-            }
-        }
-    }    
-}
-
-void selection_sort (int array[],int n) {
-  int i, j, minino, auxiliar;
-  
-  for (i = 0; i < (n - 1); i++) {
-    minino = i;
-    for (j = i+1; j < n; j++) {
-      if (array[j] < array[minino]) {
-        minino = j;
-      }
-    }
-    if (i != minino) {
-      trocar(array[i], array[minimo]);
-    }
-  }
-}
-
-
-void heap_sort(int array[], int n)
-{
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(array, n, i);
- 
-    for (int i = n - 1; i > 0; i--) {
-        trocar(array[0], array[i]);
- 
-        heapify(array, i, 0);
-    }
-}
-
-
-void heapify(int array[], int n, int i)
-{
-    int esquerda = 2 * i + 1;
-    int direita = 2 * i + 2;
-    int maior = i;
-
-    if (direita < n && array[direita] > array[maior])
-        maior = direita;
- 
-    if (esquerda < n && array[esquerda] > array[maior])
-        maior = l;
- 
- 
-    if (maior != i) {
-        trocar(array[i], array[maior]);
- 
-        heapify(array, n, maior);
-    }
-}
-
--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include "lib/myLib.h"
-#define N_LENGTH 100 
+#define N_LENGTH 20 
 
 int main(){
     int N = 0;
     
     double s_CPU_inicial, s_total_inicial, s_CPU_final, s_total_final;
+    const char methods[4][50] = {"Bubble","Selection","Heap","Box"};
 
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < 1; i++){
         N += N_LENGTH;
         int *randomArray = newArray(N);
         for(int id = 1; id <= 4; id++){
-            callFunction(randomArray, id, N, &s_CPU_inicial, &s_total_inicial, &s_CPU_final, &s_total_final);
-            printf ("Tempo de CPU total = %f\n", s_CPU_final - s_CPU_inicial);
+            int *sortedArray = callFunction(randomArray, id, N, &s_CPU_inicial, &s_total_inicial, &s_CPU_final, &s_total_final);
+            // printf ("Tempo de CPU total = %f\n", s_CPU_final - s_CPU_inicial);
+            if(sortedArray){
+                printf("Metodo %s \n", methods[id-1]);
+                printf("=========================||================\n");
+                for(int j = 0; j < N; j++){
+                    printf("%d \n", sortedArray[j]);
+                }
+            }
         }
     }
     return 0;
